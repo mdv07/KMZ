70110b2e0b60de7f2b76fb3532f646c86180c1ce
DenisM <mdv_96@mail.ru>
2016-03-13 16:41:21 +0700

    Initial Commit

---
+++ laba#1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  laba#1
+//
+//  Created by Denis on 13.03.16.
+//  Copyright © 2016 DenisM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
